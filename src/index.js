196719141e223e09842685b585e199eb1f7b6ed8
misakidrummer <nanimotooka@gmail.com>
2022-07-04 16:02:35 -0300

    primeiros commits

--- src/index.js
+++ src/index.js
@@ -16,6 +16,10 @@ import {bd} from './infra/bdSQLite-filmes.js'
 import {filmes} from './controllers/filmes-controller.js'
 filmes(app, bd)
 
+import {lientes} from '../src/controllers/clientes-controller.js'
+Clientes(app);
+
+
 app.listen(port,(port)=>{
   console.log("Porta funcionando")
 })
