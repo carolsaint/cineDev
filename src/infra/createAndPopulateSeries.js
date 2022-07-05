ea7fa8cfec961c1f50669e8d1626a6308e6b7895
Carol Saint <santocarolina98@gmail.com>
2022-07-04 22:18:42 -0300

    creating db

---
+++ src/infra/createAndPopulateSeries.js
@@ -0,0 +1,33 @@
+import sqlite3 from "sqlite3";
+const bd = new sqlite3.Database("./series.db");
+
+const SERIES_SCHEMA = `
+CREATE TABLE IF NOT EXISTS "SERIES" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "title" VARCHAR(200),
+    "description" varchar (500),
+    "genre" varchar(64),
+    "seasons" DECIMAL(100),
+);`;
+    
+const ADD_SERIES_DATA = `INSERT INTO SERIES (id, title, description, genre, seasons) VALUES (1, 'Doctor Who', 'time travelling blue box', 'sci fi', '39')`;
+
+
+function criaTabelaSeries() {
+    bd.run(SERIES_SCHEMA, (error) => {
+      if (error) console.log('Erro ao criar tabela', error);
+    });
+  }
+  
+  
+  function populaTabelaSeries() {
+    bd.run(ADD_SERIES_DATA, (error) => {
+      if (error) console.log("Erro ao popular tabela de Series");
+    });
+  }
+  
+
+  bd.serialize(() => {
+    criaTabelaSeries();
+    populaTabelaSeries();
+  });
--- src/models/series-model.js
+++ src/models/series-model.js
@@ -1,11 +1,10 @@
 let id = 0;
-export class Series {
+export class SERIES {
     constructor(title, description, genre, seasons, img ){
         this.id = id++;
         this.title = title;
         this.description = description;
         this.genre = genre;
         this.seasons = seasons;
-        this.img = img;
     }
 }
